4d375d2e8e475e0e59bfd1b52048bf93eccf3348
Herleeyandi Markoni <herleeyandi@gmail.com>
2014-07-07 18:39:58 +0700

    Template sudah jadi.

---
+++ KP Twitter Monitor/KP Twitter Monitor/Content.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace KP_Twitter_Monitor
+{
+    public partial class Content : PhoneApplicationPage
+    {
+        public Content()
+        {
+            InitializeComponent();
+        }
+        private void ApplicationBarIconButton_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+        private void ApplicationBarIconButton_Click_1(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+    }
+}
---
+++ KP Twitter Monitor/KP Twitter Monitor/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using KP_Twitter_Monitor.Resources;
+
+namespace KP_Twitter_Monitor
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
